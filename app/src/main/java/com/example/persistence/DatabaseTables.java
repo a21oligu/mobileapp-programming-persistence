99cdf90c369ed58106f85714768ebffcbbfd3fde
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-27 11:32:39 +0200

    updated create table query

--- app/src/main/java/com/example/persistence/DatabaseTables.java
+++ app/src/main/java/com/example/persistence/DatabaseTables.java
@@ -2,13 +2,13 @@ package com.example.persistence;
 
 public class DatabaseTables {
     public class Candy {
-        static final String COLUMN_NAME = "name"; // name = primary key
+        static final String COLUMN_NAME = "name"; // primary key
         static final String COLUMN_TASTE = "taste";
         static final String COLUMN_COLOR = "color";
         static final String TABLE_NAME = "Candy";
     }
 
-    public static final String SQL_CREATE_TABLE_CANDIES = String.format(
+    public static final String SQL_CREATE_TABLE_CANDY = String.format(
             "CREATE TABLE %s (%s TEXT PRIMARY KEY, %s TEXT, %s TEXT)",
             Candy.TABLE_NAME,
             Candy.COLUMN_NAME,

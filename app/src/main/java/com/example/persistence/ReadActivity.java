c1f962161b757716f69425068df9359339c0e96e
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-27 11:28:46 +0200

    added SecondActivity to display read rows from database

--- app/src/main/java/com/example/persistence/ReadActivity.java
+++ app/src/main/java/com/example/persistence/ReadActivity.java
@@ -3,12 +3,40 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ReadActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private String dataText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
+
+        textView = findViewById(R.id.textView);
+
+        Bundle extras = getIntent().getExtras();
+
+        ArrayList<String> candyStrings = extras.getStringArrayList("data");
+
+        if (extras == null || candyStrings.size() == 0) {
+            textView.setText("No content to show");
+            return;
+        };
+
+        textView.setText("");
+
+        for (String s : extras.getStringArrayList("data")) {
+            textView.setText(String.format("%s%s \n", textView.getText(), s));
+        }
+    }
+
+    public void finishActivity(View view) {
+        this.finish();
     }
 }
